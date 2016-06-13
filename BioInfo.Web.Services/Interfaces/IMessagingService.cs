d2bee6455af714d8506accbd619b87a355b8df6e
David Crook <drcrook@outlook.com>
2016-06-13 15:53:31 -0400

    Publish profiles for each application. as well as services and some interfaces.

---
+++ BioInfo.Web.Services/Interfaces/IMessagingService.cs
@@ -0,0 +1,16 @@
+﻿using BioInfo.Web.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioInfo.Web.Services
+{
+    public interface IMessagingService
+    {
+        bool AssignSecurityTokenToDevice();
+        bool RevokeSecurityTokenFromDevice();
+        bool SendDeviceNotification(IDevice device, IMessage message);
+    }
+}
--- Data/BioInfo.Domain.SQL/Band.sql
+++ Data/BioInfo.Domain.SQL/Band.sql
@@ -3,5 +3,6 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [DomainUserId] INT NULL, 
     [IsActive] BIT NOT NULL, 
-    [IoTHubKey] NVARCHAR(50) NULL
+    [IoTHubKey] NVARCHAR(255) NULL, 
+    [IoTHubId] NVARCHAR(255) NULL
 )
