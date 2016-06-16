8d5ff7f0f86faa717f121f71b33727ea63fe6bda
James Sturtevant <jsturtevant@gmail.com>
2016-06-16 10:36:32 -0400

    added tests around failed registration

--- Tests/BioInfo.Tests.ApplicationApi/DeviceAdminControllerTests.cs
+++ Tests/BioInfo.Tests.ApplicationApi/DeviceAdminControllerTests.cs
@@ -35,5 +35,22 @@ namespace BioInfo.Tests.ApplicationApi
     
             Assert.IsInstanceOfType(result, typeof(OkNegotiatedContentResult<bool>));
         }
+
+        [TestMethod]
+        public async Task Register_device_should_return_bad_request()
+        {
+            var deviceRegistration = new Mock<IDeviceRegistration>();
+            deviceRegistration.Setup(x => x.RegisterDeviceAsync(It.IsAny<IDevice>())).Returns(Task.FromResult(new FunctionResult<bool>(false, "error message",true)));
+
+            var adminController = new DeviceAdminController(deviceRegistration.Object);
+
+            IHttpActionResult result = await adminController.RegisterDevice(new Device());
+
+
+            Assert.IsInstanceOfType(result, typeof(BadRequestErrorMessageResult));
+            var badRequest = result as BadRequestErrorMessageResult;
+
+            Assert.AreEqual(badRequest.Message, "error message");
+        }
     }
 }
