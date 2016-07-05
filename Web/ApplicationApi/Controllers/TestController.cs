c9c6dbc366ea9c7416438b0bfeddcccc6060a4fb
David Crook <drcrook@outlook.com>
2016-07-05 15:41:03 -0400

    Test Controller for Auth, looks good.  All works!

---
+++ Web/ApplicationApi/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace BioInfo.Web.ApplicationApi.Controllers
+{
+    [RoutePrefix("api/Test")]
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        [Authorize]
+        public async Task<IHttpActionResult> Get()
+        {
+            return Ok("It Works!");
+        }
+    }
+}
